8123bf06dab5dae7934df0d6480664a5e6142f61
Andre Thomas <andre.thomas@modea.com>
2017-12-26 14:35:04 -0500

    Initial translation en-de

--- src/build/js/script.js
+++ src/build/js/script.js
@@ -1,37 +1,46 @@
 'use strict';
 
+var apiKey = 'trnsl.1.1.20171226T172043Z.e378d286d4be7d8e.3b8cb91c3c8a2e890d1dc77cd47f198e0218b9c9';
 var translateButton = document.getElementById('translate-button');
-var languages = [];
-var languagesObj = void 0;
-
-function translate() {
-    console.log('Translating...');
-}
+var languages = void 0;
 
 function constructor() {
     setEventListeners();
     getLanguages();
 }
 
-function getLanguages() {
-    var apiKey = 'trnsl.1.1.20171226T172043Z.e378d286d4be7d8e.3b8cb91c3c8a2e890d1dc77cd47f198e0218b9c9';
+function setEventListeners() {
+    translateButton.addEventListener('click', translate);
+}
 
+function getLanguages() {
     fetch('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=' + apiKey + '&ui=en').then(function (res) {
         res.json().then(function (data) {
-            languagesObj = data.langs;
-            for (var key in data.langs) {
-                if (data.langs.hasOwnProperty(key)) {
-                    languages.push(key);
-                }
-            }
+            languages = data.langs;
+            console.log(languages);
+            // for (let key in data.langs) {
+            //     if (data.langs.hasOwnProperty(key)) {
+            //         languages.push(key);
+            //     }
+            // }
         });
     }).catch(function (err) {
         console.log('Error: ' + err);
     });
 }
 
-function setEventListeners() {
-    translateButton.addEventListener('click', translate);
+function translate() {
+    var textToTranslate = document.getElementById('text-to-translate').value;
+    var translatedText = document.getElementById('translated-text');
+    var translateFromTo = 'de-en';
+
+    fetch('https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + apiKey + '&text=' + textToTranslate + '&lang=' + translateFromTo + '&format=plain').then(function (res) {
+        res.json().then(function (data) {
+            translatedText.value = data.text[0];
+        });
+    }).catch(function (err) {
+        console.log('Error: ' + err);
+    });
 }
 
 constructor();
--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,27 @@
+let apiKey = 'trnsl.1.1.20171226T172043Z.e378d286d4be7d8e.3b8cb91c3c8a2e890d1dc77cd47f198e0218b9c9';
 let translateButton = document.getElementById('translate-button');
-let languages = [];
-let languagesObj;
-
-function translate() {
-    console.log('Translating...');
-}
+let languages;
 
 function constructor() {
     setEventListeners();
     getLanguages();
 }
 
-function getLanguages() {
-    let apiKey = 'trnsl.1.1.20171226T172043Z.e378d286d4be7d8e.3b8cb91c3c8a2e890d1dc77cd47f198e0218b9c9';
+function setEventListeners() {
+    translateButton.addEventListener('click', translate);
+}
 
+function getLanguages() {
     fetch('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=' + apiKey + '&ui=en')
         .then((res) => {
             res.json().then((data) => {
-                languagesObj = data.langs;
-                for (let key in data.langs) {
-                    if (data.langs.hasOwnProperty(key)) {
-                        languages.push(key);
-                    }
-                }
+                languages = data.langs;
+                console.log(languages);
+                // for (let key in data.langs) {
+                //     if (data.langs.hasOwnProperty(key)) {
+                //         languages.push(key);
+                //     }
+                // }
             })
         })
         .catch((err) => {
@@ -30,8 +29,22 @@ function getLanguages() {
         });
 }
 
-function setEventListeners() {
-    translateButton.addEventListener('click', translate);
+function translate() {
+    let textToTranslate = document.getElementById('text-to-translate').value;
+    let translatedText = document.getElementById('translated-text');
+    let translateFromTo = 'de-en';
+    
+    fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${apiKey}&text=${textToTranslate}&lang=${translateFromTo}&format=plain`)
+        .then((res) => {
+            res.json().then((data) => {
+                translatedText.value = data.text[0];
+            })
+        })
+        .catch((err) => {
+            console.log('Error: ' + err);
+        });
 }
+
+
 
 constructor();
