d27124e9de712bbab3cf52fe3c0d5d9c56377de3
Andre Thomas <andre.thomas@modea.com>
2017-12-27 12:40:24 -0500

    Allow user to select both languages

--- src/build/js/script.js
+++ src/build/js/script.js
@@ -1,6 +1,10 @@
 'use strict';
 
 var apiKey = 'trnsl.1.1.20171226T172043Z.e378d286d4be7d8e.3b8cb91c3c8a2e890d1dc77cd47f198e0218b9c9';
+var textArea1 = document.getElementById('textArea1');
+var textArea2 = document.getElementById('textArea2');
+var languageSelection1 = document.getElementById('language-selection1');
+var languageSelection2 = document.getElementById('language-selection2');
 var translateButton = document.getElementById('translate-button');
 var languages = {};
 
@@ -16,6 +20,7 @@ function setEventListeners() {
 function getLanguages() {
     fetch('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=' + apiKey + '&ui=en').then(function (res) {
         res.json().then(function (data) {
+            languages = data.langs;
             populateLanguageSelection(data.langs);
         });
     }).catch(function (err) {
@@ -23,37 +28,46 @@ function getLanguages() {
     });
 }
 
-function populateLanguageSelection(languages) {
-    var languageSelection1 = document.getElementById('language-selection1');
-    var languageSelection2 = document.getElementById('language-selection2');
-
+function populateLanguageSelection() {
     for (var key in languages) {
         if (languages.hasOwnProperty(key)) {
-            var option1 = document.createElement('option');
-            option1.setAttribute('value', languages[key]);
-            option1.appendChild(document.createTextNode(languages[key]));
-            languageSelection1.appendChild(option1);
+            var option = document.createElement('option');
+            option.setAttribute('value', languages[key]);
+            option.appendChild(document.createTextNode(languages[key]));
 
-            var option2 = document.createElement('option');
-            option2.setAttribute('value', languages[key]);
-            option2.appendChild(document.createTextNode(languages[key]));
-            languageSelection2.appendChild(option2);
+            languageSelection1.appendChild(option);
+            languageSelection2.appendChild(option.cloneNode(true));
         }
     }
 }
 
 function translate() {
-    var textToTranslate = document.getElementById('text-to-translate').value;
-    var translatedText = document.getElementById('translated-text');
-    var translateFromTo = 'de-en';
+    if (languageSelection1.selectedIndex > 0 && languageSelection2.selectedIndex > 0 && textArea1.value) {
+        var textToTranslate = textArea1.value;
+        var selectedLanguage1 = languages.getKeyByValue(languageSelection1.options[languageSelection1.selectedIndex].value);
+        var selectedLanguage2 = languages.getKeyByValue(languageSelection2.options[languageSelection2.selectedIndex].value);
+        var translateFromTo = selectedLanguage1 + '-' + selectedLanguage2;
 
-    fetch('https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + apiKey + '&text=' + textToTranslate + '&lang=' + translateFromTo + '&format=plain').then(function (res) {
-        res.json().then(function (data) {
-            translatedText.value = data.text[0];
+        fetch('https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + apiKey + '&text=' + textToTranslate + '&lang=' + translateFromTo + '&format=plain').then(function (res) {
+            res.json().then(function (data) {
+                if (data.code === 200) {
+                    textArea2.value = data.text[0];
+                }
+            });
+        }).catch(function (err) {
+            console.log('Error: ' + err);
         });
-    }).catch(function (err) {
-        console.log('Error: ' + err);
-    });
+    } else {
+        // TODO: Display error message
+    }
 }
+
+Object.prototype.getKeyByValue = function (value) {
+    for (var prop in this) {
+        if (this.hasOwnProperty(prop)) {
+            if (this[prop] === value) return prop;
+        }
+    }
+};
 
 main();
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,8 @@
 let apiKey = 'trnsl.1.1.20171226T172043Z.e378d286d4be7d8e.3b8cb91c3c8a2e890d1dc77cd47f198e0218b9c9';
+let textArea1 = document.getElementById('textArea1');
+let textArea2 = document.getElementById('textArea2');
+let languageSelection1 = document.getElementById('language-selection1');
+let languageSelection2 = document.getElementById('language-selection2');
 let translateButton = document.getElementById('translate-button');
 let languages = {};
 
@@ -15,47 +19,59 @@ function getLanguages() {
     fetch('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=' + apiKey + '&ui=en')
         .then((res) => {
             res.json().then((data) => {
+                languages = data.langs;
                 populateLanguageSelection(data.langs);
             })
         })
         .catch((err) => {
             console.log('Error: ' + err);
-        });
+        }
+    );
 }
 
-function populateLanguageSelection(languages) {
-    let languageSelection1 = document.getElementById('language-selection1');
-    let languageSelection2 = document.getElementById('language-selection2');
-    
+function populateLanguageSelection() {
     for (let key in languages) {
         if (languages.hasOwnProperty(key)) {
-            let option1 = document.createElement('option');
-            option1.setAttribute('value', languages[key]);
-            option1.appendChild(document.createTextNode(languages[key]));
-            languageSelection1.appendChild(option1);
-
-            let option2 = document.createElement('option');
-            option2.setAttribute('value', languages[key]);
-            option2.appendChild(document.createTextNode(languages[key]));
-            languageSelection2.appendChild(option2);
+            let option = document.createElement('option');
+            option.setAttribute('value', languages[key]);
+            option.appendChild(document.createTextNode(languages[key]));
+            
+            languageSelection1.appendChild(option);
+            languageSelection2.appendChild(option.cloneNode(true));
         }
     }
 }
 
 function translate() {
-    let textToTranslate = document.getElementById('text-to-translate').value;
-    let translatedText = document.getElementById('translated-text');
-    let translateFromTo = 'de-en';
-    
-    fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${apiKey}&text=${textToTranslate}&lang=${translateFromTo}&format=plain`)
-        .then((res) => {
-            res.json().then((data) => {
-                translatedText.value = data.text[0];
+    if (languageSelection1.selectedIndex > 0 && languageSelection2.selectedIndex > 0 && textArea1.value) {
+        let textToTranslate = textArea1.value;
+        let selectedLanguage1 = languages.getKeyByValue(languageSelection1.options[languageSelection1.selectedIndex].value);
+        let selectedLanguage2 = languages.getKeyByValue(languageSelection2.options[languageSelection2.selectedIndex].value);
+        let translateFromTo = selectedLanguage1 + '-' + selectedLanguage2;
+        
+        fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${apiKey}&text=${textToTranslate}&lang=${translateFromTo}&format=plain`)
+            .then((res) => {
+                res.json().then((data) => {
+                    if (data.code === 200) {
+                        textArea2.value = data.text[0];
+                    }
+                })
             })
-        })
-        .catch((err) => {
-            console.log('Error: ' + err);
-        });
+            .catch((err) => {
+                console.log('Error: ' + err);
+            });
+    } else {
+        // TODO: Display error message
+    }
+}
+
+Object.prototype.getKeyByValue = function(value) {
+    for(var prop in this) {
+        if(this.hasOwnProperty(prop)) {
+             if(this[prop] === value)
+                 return prop;
+        }
+    }
 }
 
 main();
